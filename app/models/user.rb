f0104b40ff9beaa539ffc0e3f676df6ff917294d
nyastassia <128276412+nyastassia@users.noreply.github.com>
2025-06-11 17:21:46 +0900

    Add associations to User model for comments and saved wineries

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+has_many :comments
+has_many :saved_wineries
+has_many :wineries, through: :saved_wineries
 end
