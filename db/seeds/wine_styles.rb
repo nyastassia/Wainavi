828fe572ddb96af4065cb6cb1b7b409f9367c677
nyastassia <128276412+nyastassia@users.noreply.github.com>
2025-07-07 20:42:08 +0900

    Add seed data for production wine_styles, experiences, dinings, awards

--- db/seeds/wine_styles.rb
+++ db/seeds/wine_styles.rb
@@ -1,10 +1,10 @@
-# ⚪️ ワインスタイル（Wine Styles）
+⚪️ ワインスタイル（Wine Styles）
 
-#WineStyle.find_or_create_by!(name: "スパークリング")   # Sparkling
-#WineStyle.find_or_create_by!(name: "白ワイン（辛口）") # White (Dry)
-#WineStyle.find_or_create_by!(name: "白ワイン（甘口）") # White (Sweet)
-#WineStyle.find_or_create_by!(name: "ロゼ")             # Rosé
-#WineStyle.find_or_create_by!(name: "赤ワイン（ライトボディ）") # Red (Light body)
-#WineStyle.find_or_create_by!(name: "赤ワイン（フルボディ）")   # Red (Full body)
-#WineStyle.find_or_create_by!(name: "デザートワイン")   # Dessert wine
-#WineStyle.find_or_create_by!(name: "オレンジワイン")   # Orange wine
+WineStyle.find_or_create_by!(name: "スパークリング")   # Sparkling
+WineStyle.find_or_create_by!(name: "白ワイン（辛口）") # White (Dry)
+WineStyle.find_or_create_by!(name: "白ワイン（甘口）") # White (Sweet)
+WineStyle.find_or_create_by!(name: "ロゼ")             # Rosé
+WineStyle.find_or_create_by!(name: "赤ワイン（ライトボディ）") # Red (Light body)
+WineStyle.find_or_create_by!(name: "赤ワイン（フルボディ）")   # Red (Full body)
+WineStyle.find_or_create_by!(name: "デザートワイン")   # Dessert wine
+WineStyle.find_or_create_by!(name: "オレンジワイン")   # Orange wine
